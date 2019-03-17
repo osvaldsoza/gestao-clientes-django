a77b91a8d9f1bc20af64e0270cbf9d8bf2637712
osvaldsoza <osvald.soza@gmail.com>
2019-03-17 13:51:05 -0300

    Atualização 2 - conenctando a[locação ao posgres

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'gestao_clientes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gestao_clientes',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
